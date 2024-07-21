2bd390fd81cf6888e267649fd80ded1e999b6f95
Ana-Zhizhiashvili <ana.zhizhiashvili@twino.eu>
2024-07-21 15:12:04 +0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import SingleLinkedList.SingleLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        list.printList();
+        list.push(1);
+        list.printList();
+        list.push(2);
+        list.printList();
+    }
+
+}
